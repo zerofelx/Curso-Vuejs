c51ada2d0311606916938c0874c2a6071ed93813
Zerofelx <zeroflex3@gmail.com>
2020-01-06 04:09:02 -0500

    Terminando proyecto "Cyroz Exchange"

--- App 2/cyroz-exchange/src/App.vue
+++ App 2/cyroz-exchange/src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <main>
-    <CxHeader></CxHeader>
+    <cx-header :links="links" />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
 </template>
 
 <script>
-import CxHeader from "@/components/CxHeader";
+import CxHeader from '@/components/CxHeader'
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     CxHeader
+  },
+  data() {
+    return {
+      links: [
+        {
+          title: 'BTC',
+          to: { name: 'coin-detail', params: { id: 'bitcoin' } }
+        },
+        {
+          title: 'ETH',
+          to: { name: 'coin-detail', params: { id: 'ethereum' } }
+        },
+        {
+          title: 'XRP',
+          to: { name: 'coin-detail', params: { id: 'ripple' } }
+        },
+        {
+          title: 'LTC',
+          to: { name: 'coin-detail', params: { id: 'litecoin' } }
+        }
+      ]
+    };
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
