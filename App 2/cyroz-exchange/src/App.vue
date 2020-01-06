e1958371fb71efff11a3caf09b4b6caea768daee
Zerofelx <zeroflex3@gmail.com>
2020-01-05 22:10:50 -0500

    Agregando vue npm

---
+++ App 2/cyroz-exchange/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <CxHeader></CxHeader>
+  </main>
+</template>
+
+<script>
+import CxHeader from "@/components/CxHeader";
+
+export default {
+  name: "app",
+  components: {
+    CxHeader
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
